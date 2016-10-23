b86986e68742beb79fe953136566816f638e3105
sirlarryton <jrk123a@gmail.com>
2016-10-23 19:41:23 -0400

    rappy update 2
    cause for some reason the first one didn't go through.

--- input1_MarioHerrera.py
+++ input1_MarioHerrera.py
@@ -1,11 +1,12 @@
 #THIS PROGRAM ADDS THREE NUMBERS
 
-# input three variables a, b, and c
+# input three variables a, b, c, and rappy
 a = 4
 b = 7
 c = 3
-## calculcate the sum of a, b and c 
-result = a + b + c
+rappy = 6
+## calculcate the sum of a, b, c and rappy 
+result = a + b + c + rappy
 
 ##display the result
 print(result)
