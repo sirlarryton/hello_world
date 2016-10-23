efd460e8ab6f170bb4b3779a79b33452bad9b3c5
sirlarryton <jrk123a@gmail.com>
2016-10-23 19:38:56 -0400

    rappy update try 2
    cause it seems the first one disappeared for some reason.

--- input1_MarioHerrera.py
+++ input1_MarioHerrera.py
@@ -1,11 +1,12 @@
 #THIS PROGRAM ADDS THREE NUMBERS
 
-# input three variables a, b, and c
+# input three variables a, b, c, and, rappy
 a = 4
 b = 7
 c = 3
+rappy= 6
 ## calculcate the sum of a, b and c 
-result = a + b + c
+result = a + b + c + rappy
 
 ##display the result
 print(result)
